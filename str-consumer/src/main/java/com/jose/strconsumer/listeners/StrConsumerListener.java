913c68d3ca82353a72ce7d8f9ccc341ec03f70a1
jose <jose.maria.mangueira@gmail.com>
2023-07-31 15:30:51 -0300

    record interceptor

--- str-consumer/src/main/java/com/jose/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/jose/strconsumer/listeners/StrConsumerListener.java
@@ -1,6 +1,7 @@
 package com.jose.strconsumer.listeners;
 
 import com.jose.strconsumer.custom.StrConsumerCustomListener;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
@@ -11,14 +12,18 @@ public class StrConsumerListener {
 
     @StrConsumerCustomListener(groupId = "group-1")
     public void create(String message){
+
         log.info("CREATE ::: Receive message {}", message);
     }
     @StrConsumerCustomListener(groupId = "group-1")
     public void log(String message){
         log.info("LOG ::: Receive message {}", message);
     }
-    @StrConsumerCustomListener(groupId = "group-2")
+   @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validMessageContainerFactory")
+   //@StrConsumerCustomListener(groupId = "group-2")
     public void history(String message){
+
+
         log.info("HISTORY ::: Receive message {}", message);
     }
 }
