be000eb1e25e6eaa3342544461b57e6c7a262948
jose <jose.maria.mangueira@gmail.com>
2023-07-31 09:14:18 -0300

    str consumer created

---
+++ str-consumer/src/main/java/com/jose/strconsumer/listeners/StrConsumerListener.java
@@ -0,0 +1,17 @@
+package com.jose.strconsumer.listeners;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+@Log4j2
+@Component
+public class StrConsumerListener {
+    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void create(String message) {
+        log.info("CREATE ::: Receive message {}", message);
+    }
+    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void log(String message){
+        log.info("LOG ::: Receive message {}", message);
+    }
+}
