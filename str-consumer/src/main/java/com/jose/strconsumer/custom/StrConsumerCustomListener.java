2087b52fb69a26b785579bbf6e8e06bc3b2f13d6
jose <jose.maria.mangueira@gmail.com>
2023-08-01 07:51:05 -0300

    error handler

--- str-consumer/src/main/java/com/jose/strconsumer/custom/StrConsumerCustomListener.java
+++ str-consumer/src/main/java/com/jose/strconsumer/custom/StrConsumerCustomListener.java
@@ -18,5 +18,7 @@ public @interface StrConsumerCustomListener {
     String containerFactory() default "strContainerFactory";
     @AliasFor(annotation = KafkaListener.class, attribute = "groupId")
     String groupId() default "";
+    @AliasFor(annotation = KafkaListener.class, attribute = "errorHandler")
+    String errorHandler() default "errorCustomHandler";
 
 }
